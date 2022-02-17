040011787c3cc449428a739496714163a3253d14
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2022-02-17 14:50:09 +0100

    operational route

--- back/app.js
+++ back/app.js
@@ -1,11 +1,12 @@
 const controllers = require("./api");
-const rcon = require('rcon');
 const express = require('express');
 const router = express.Router();
 
 async function startServer() {
   const app = express();
   const port = 3000;
+
+  await require("./loaders")(app);
 
   app.listen(port, (err) => {
     if (err) {
@@ -23,28 +24,5 @@ async function startServer() {
 
 startServer();
 
-/** RCON */
-var options = {
-  tcp: true,       // false for UDP, true for TCP (default true)
-  challenge: true  // true to use the challenge protocol (default true)
-};
 
-let conn = new rcon('82.64.160.64', 25575, 'rcon', options);
-conn.send("say test")
 
-conn.on('auth', function() {
-  // You must wait until this event is fired before sending any commands,
-  // otherwise those commands will fail.
-  console.log("Authenticated");
-  console.log("Sending command: say test");
-  conn.send("say test");
-}).on('response', function(str) {
-  console.log("Response: " + str);
-}).on('error', function(err) {
-  console.log("Error: " + err);
-}).on('end', function() {
-  console.log("Connection closed");
-  process.exit();
-});
-
-conn.connect();
