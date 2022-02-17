86d05e4f561cd63686364c01cea1cfbfb03d7ad2
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2022-02-17 13:03:04 +0100

    not working rcon nodejs

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const Rcon = require('../node-rcon');
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -12,3 +14,23 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+/** RCON */
+let conn = new Rcon('localhost', 3000, 'password');
+
+conn.on('auth', function() {
+  // You must wait until this event is fired before sending any commands,
+  // otherwise those commands will fail.
+  console.log("Authenticated");
+  console.log("Sending command: help")
+  conn.send("help");
+}).on('response', function(str) {
+  console.log("Response: " + str);
+}).on('error', function(err) {
+  console.log("Error: " + err);
+}).on('end', function() {
+  console.log("Connection closed");
+  process.exit();
+});
+
+conn.connect();
