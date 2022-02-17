7c3e0643a99b225f70d6d3fc3c3c60273059e191
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2022-02-17 14:50:20 +0100

    operational route

--- back/api/routes/main.js
+++ back/api/routes/main.js
@@ -1,10 +1,45 @@
 const express = require('express');
+const { route } = require('express/lib/application');
 const router = express.Router();
+const rcon = require('rcon');
 
 module.exports = () => {
+    /** RCON */
+    const options = {
+        tcp: true,       // false for UDP, true for TCP (default true)
+        challenge: true  // true to use the challenge protocol (default true)
+    };
+
     router.get('/', async (req, res) => {
         
-        res.send("Get Test")
+        res.send("test")
     });
 
-}
+    router.post('/', async (req, res) => {
+
+        let conn = new rcon('82.64.160.64', 25575, 'rcon', options);
+
+            conn.on('auth', function() {
+            // You must wait until this event is fired before sending any commands,
+            // otherwise those commands will fail.
+            console.log("Authenticated");
+            console.log("Affichage de la commande reçue : ", JSON.stringify(req.body.command));
+            conn.send(req.body.command);
+            // conn.send("help");
+        }).on('response', function(str) {
+            console.log("Response: " + str);
+        }).on('error', function(err) {
+            console.log("Error: " + err);
+        }).on('end', function() {
+            console.log("Connection closed");
+            process.exit();
+        });
+
+        conn.connect();
+        
+        // res.send(res)
+    });
+
+    return router;
+};
+
