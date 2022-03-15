4e2531df9f6d198c12c8b6cb7fc2fcb918bd309b
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2022-03-15 17:21:47 +0100

    Start JWT auth (not fully operational yet)

--- back/api/index.js
+++ back/api/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const main = require('./routes/main');
 const router = express.Router();
 
 module.exports = () => {
-    router.use('/main', main());
+    router.use('/main', auth, main());
 
     return router;
 };
---
+++ back/middleware/roles.js
@@ -0,0 +1,9 @@
+module.exports = (roles) => {
+    return (req, res, next) => {
+        if (roles.includes(req.user.role)) {
+            next();
+        } else {
+            res.sendStatus(403);
+        }
+    }
+}
