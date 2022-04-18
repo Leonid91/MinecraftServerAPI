ecf43076a60fdef545354bcdcb410be947b3a509
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2022-04-18 20:08:37 +0200

    Add some inventory routes

--- back/api/index.js
+++ back/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // const auth = require('../middleware/auth');
 const main = require('./routes/main');
+const inventory = require('./routes/inventory');
 const whitelist = require('./routes/whitelist');
 const rcon = require('./routes/rcon');
 const router = express.Router();
@@ -9,6 +10,7 @@ module.exports = () => {
     router.use('/main', main());
     router.use('/whitelist', whitelist());
     router.use('/rcon', rcon());
+    router.use('/inventory', inventory());
 
     return router;
 };
