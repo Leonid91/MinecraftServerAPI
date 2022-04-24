8a577a4ba9e069177364ce72755d86efed19b0ed
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2022-04-24 13:01:02 +0200

    auth routes code rewriting

--- back/api/index.js
+++ back/api/index.js
@@ -5,12 +5,14 @@ const inventory = require('./routes/inventory');
 const whitelist = require('./routes/whitelist');
 const rcon = require('./routes/rcon');
 const router = express.Router();
+const user = require('./routes/user');
 
 module.exports = () => {
     router.use('/main', main());
     router.use('/whitelist', whitelist());
     router.use('/rcon', rcon());
     router.use('/inventory', inventory());
+    router.use('/user', user());
 
     return router;
 };
